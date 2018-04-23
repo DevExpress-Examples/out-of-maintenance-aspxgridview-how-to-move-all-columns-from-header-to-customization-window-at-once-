a3c60d4402fc4473e7c4fe2f29fd93b9daa4b9e4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ASPxGridView - How to move all columns from Header to Customization Window at once
+
+
+<p>The example demonstrates how to move all columns from Header to Customization Window (and reverse) at once by clicking the external button. The button's state is synchronized automatically based on the state of the ASPxGridView's Columns.</p><p><strong>See Also:<br />
+</strong><a href="https://www.devexpress.com/Support/Center/p/E3033">ASPxGridView - How to trace the process of dragging columns  to Customization Window and within a Header</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,33 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub grid_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		Dim gridView As ASPxGridView = CType(sender, ASPxGridView)
+		For Each column As GridViewColumn In gridView.Columns
+			If Convert.ToBoolean(hf("columnsInWindow")) Then
+				If column.Visible AndAlso column.ShowInCustomizationForm Then
+					column.Visible = False
+				End If
+			Else
+				If (Not column.Visible) AndAlso column.ShowInCustomizationForm Then
+					column.Visible = True
+				End If
+			End If
+		Next column
+	End Sub
+	Protected Sub grid_AfterPerformCallback(ByVal sender As Object, ByVal e As ASPxGridViewAfterPerformCallbackEventArgs)
+		If e.CallbackName = "COLUMNMOVE" Then
+			Dim gridView As ASPxGridView = CType(sender, ASPxGridView)
+			gridView.JSProperties("cpColumnsInHeader") = False
+			For Each column As GridViewColumn In gridView.Columns
+				If column.Visible AndAlso column.ShowInCustomizationForm Then
+					gridView.JSProperties("cpColumnsInHeader") = True
+					Exit For
+				End If
+			Next column
+		End If
+	End Sub
+End Class
